91eb399d5fbfda2fe26c925f9ea779d0d4250b0d
KanVull <minirorus7@gmail.com>
2021-10-08 18:28:24 +0300

    commented

--- main2.py
+++ main2.py
@@ -1,21 +1,28 @@
 import random as rnd
 
 def quick_sort(array):  
+    # Функция сортировки массива
 
-    def partition(array, left, right):  
-        x = rnd.choice( array[left:right] )
-        i, j = left, right
+    def partition(array, left, right): 
+        # Функция выбора случайного элемента-разделителя массива 
+        # (части массива от left границы до right границы) на 
+        # две части 
+        x = rnd.choice( array[left:right] ) # Выбираем случайное число в части массива
         while True:
-            while array[i] < x: i += 1
-            while array[j] > x: j -= 1
+            while array[left] < x: left += 1    #   Пока "левый" элемент меньше, чем случайно выбранный, сдвигаем границу
+                                                #   Ищем элемент, который будет больше x    
+            while array[right] > x: right -= 1  #   Такая же ситуация только с другой стороны
 
-            if i >= j:  return j
+            if left >= right:  return right     #   Прерываем цикл и возвращаем новую границу-разделитель массива
 
-            array[i], array[j] = array[j], array[i]
-            i, j = i + 1, j - 1
+            array[left], array[right] = array[right], array[left] # Меняем местами левое и правое число - таким образом справа от "x" 
+                                                                  # будет число больше "х", а слева меньше
+            left, right = left + 1, right - 1   #   Сдвигаем границы для поиска новых элементов, не проходящих по условию
+                                                #   слева от х числа меньше х, а справа больше
 
     def _quick_sort(array, left, right):
-        if left < right:
+        # Функция для запуска рекурсивной сортировки для выбранной области массива
+        if left < right: # условие продолжения рекурсии
             split_index = partition(array, left, right)
             _quick_sort(array, left, split_index)
             _quick_sort(array, split_index + 1, right)
